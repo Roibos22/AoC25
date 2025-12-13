d375466cee2722c90a9fdd52287ee4fcd7938307
LofiLeon <grimmeisen.leon@gmail.com>
2025-12-14 00:44:31 +0100

    day_06 part_02

--- day_06/day_06.py
+++ day_06/day_06.py
@@ -3,26 +3,50 @@ import sys
 test = True if len(sys.argv) == 2 and sys.argv[1] == "test" else False
 filename = "input_test.txt" if test else "input.txt"
 
-def part_one(data):
+
+def do_calc(line, op):
     res = 0
-    data = zip(*data)
-    for line in data:
-        operator = line[-1]
-        line = list(map(int, line[:-1]))
-        if operator == '+':
-            res += sum(line)
-        elif operator == '*':
-            product = 1
-            for num in line:
-                product *= num
-            res += product
+    if op == '+':
+        res += sum(line)
+    elif op == '*':
+        product = 1
+        for num in line:
+            product *= num
+        res += product
     return res
 
+
+def part_one(data):
+    res = 0
+    data = list(line.split() for line in data)
+    data = list(zip(*data))
+    for line in data:
+        op = line[-1]
+        line = list(map(int, line[:-1]))
+        res += do_calc(line, op)
+    return res
+
+
 def part_two(data):
-    pass
+    res = 0
+    ops = data[-1].split()
+    data = list(line for line in data[:-1])
+    data = list(zip(*data))
+    data = list(''.join(line).strip() for line in data)
+
+    op_i = 0
+    curr = []
+    for i, num in enumerate(data):
+        if num != '':
+            curr.append(num)
+        if num == '' or i == len(data) - 1:
+            res += do_calc(list(map(int, curr)), ops[op_i])
+            op_i += 1
+            curr = []
+    return res
 
 def main():
-    data = [line.split() for line in open(filename).read().splitlines()]
+    data = open(filename).read().splitlines()
     print("Part 1 -> ", part_one(data))
     print("Part 2 -> ", part_two(data))
 

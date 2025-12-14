5622ef14cba5ecd34d0932d201db7c8cf9934f76
LofiLeon <grimmeisen.leon@gmail.com>
2025-12-14 18:39:20 +0100

    day_07 part_02

--- day_07/day_07.py
+++ day_07/day_07.py
@@ -3,34 +3,42 @@ import sys
 test = True if len(sys.argv) == 2 and sys.argv[1] == "test" else False
 filename = "input_test.txt" if test else "input.txt"
 
+data = list(list(row) for row in open(filename).read().splitlines())
+data[0] = [s.replace('S', '|') for s in data[0]]
+
+
 def part_one(data):
-    beams = [data[0].index('S')]
     res = 0
-    for i, _ in enumerate(data[1]):
-        if data[0][i] == 'S':
-            data[1][i] = '|'
-    for y, row in enumerate(data):
-        if y == 0:
-            continue
-        for x, _ in enumerate(row):
-            if data[y-1][x] == '|':
-                if data[y][x] == '^':
+    for r, row in enumerate(data):
+        for c, _ in enumerate(row):
+            if data[r-1][c] == '|':
+                if data[r][c] == '^':
                     res+=1
-                    data[y][x-1] = '|'
-                    data[y][x+1] = '|'
+                    data[r][c-1] = '|'
+                    data[r][c+1] = '|'
                 else:
-                    data[y][x] = '|'
-        print(row)
+                    data[r][c] = '|'
     return res
 
+
 def part_two(data):
-    pass
+    cache = {}
 
-def main():
-    data = list(list(row) for row in open(filename).read().splitlines())
-    print("Part 1 -> ", part_one(data))
-    print("Part 2 -> ", part_two(data))
+    def solve(r, c):
+        if r >= len(data):
+            return 1
+        if (r, c) in cache:
+            return cache[(r, c)]
+        res = 0
+        if data[r][c] == '^':
+            res = solve(r+1,c+1) + solve(r+1,c-1)
+        elif data[r][c] == '.'  or data[r][c] == '|':
+            res = solve(r+1, c)
+        cache[(r, c)] = res
+        return res
+    
+    return solve(0, data[0].index('|'))
 
-main()
 
-
+print("Part 1 -> ", part_one(data.copy()))
+print("Part 2 -> ", part_two(data.copy()))
